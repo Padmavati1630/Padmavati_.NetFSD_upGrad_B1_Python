fa69ef91bb98e94e05d2f4bf7b1794c119f0dbb8
Padmavati M Shivanagutti <padmashivanagutti90@gmail.com>
2026-04-17 10:22:32 +0530

    Added other migration files

---
+++ E-Learning-Platform-FullStack/Backend/ElearningAPI/Migrations/20260415034847_FixCourseModel.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ElearningAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixCourseModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Title",
+                table: "Courses",
+                newName: "Name");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Courses",
+                newName: "Title");
+        }
+    }
+}
---
+++ E-Learning-Platform-FullStack/Backend/ElearningAPI/Migrations/20260416071521_MakeQuizIdNullable.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ElearningAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeQuizIdNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Results_Quizzes_QuizId",
+                table: "Results");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "QuizId",
+                table: "Results",
+                type: "INTEGER",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Results_Quizzes_QuizId",
+                table: "Results",
+                column: "QuizId",
+                principalTable: "Quizzes",
+                principalColumn: "QuizId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Results_Quizzes_QuizId",
+                table: "Results");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "QuizId",
+                table: "Results",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "INTEGER",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Results_Quizzes_QuizId",
+                table: "Results",
+                column: "QuizId",
+                principalTable: "Quizzes",
+                principalColumn: "QuizId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
